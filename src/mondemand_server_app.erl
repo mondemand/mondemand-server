f8fe21c7f3ed00b12a649adabe035e7c6213e8dc
Anthony Molinaro <anthony.molinaro@openx.com>
2014-05-19 13:52:32 -0700

    update deps to start mondemand

--- src/mondemand_server_app.erl
+++ src/mondemand_server_app.erl
@@ -13,7 +13,7 @@
 %%====================================================================
 start () ->
   Apps = lists:append ( [ [ sasl, lwes, mondemand, crypto, gproc,
-                            sidejob, afunix, inets, asn1, public_key,
+                            afunix, inets, asn1, public_key,
                             ssl, xmerl, compiler, syntax_tools, mochiweb,
                             webmachine ],
                           mondemand_server_config:applications_to_start(),
