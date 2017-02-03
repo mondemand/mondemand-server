a19bc90b7a239ea64d0a1e1349fc37d303f45e83
Anthony Molinaro <anthonym@alumni.caltech.edu>
2017-02-03 01:01:45 +0000

    Add a sink backend
    It does nothing just throws away the event

---
+++ src/mondemand_backend_sink.erl
@@ -0,0 +1,92 @@
+-module (mondemand_backend_sink).
+
+-include_lib ("lwes/include/lwes.hrl").
+-include_lib ("kernel/include/file.hrl").
+-include_lib ("mondemand/include/mondemand.hrl").
+
+-behaviour (supervisor).
+-behaviour (mondemand_server_backend).
+-behaviour (mondemand_backend_worker).
+
+%% mondemand_backend_worker callbacks
+-export ([ create/1,
+           connected/1,
+           connect/1,
+           send/2,
+           destroy/1
+         ]).
+
+%% mondemand_backend callbacks
+-export ([ start_link/1,
+           process/1,
+           required_apps/0,
+           type/0
+         ]).
+
+%% supervisor callbacks
+-export ([ init/1 ]).
+
+-define (POOL, md_be_sink_pool).
+-record (state, { }).
+
+%%====================================================================
+%% mondemand_backend callbacks
+%%====================================================================
+start_link (Config) ->
+  supervisor:start_link ( { local, ?MODULE }, ?MODULE, [Config]).
+
+process (Event) ->
+  mondemand_backend_worker_pool_sup:process (?POOL, Event).
+
+required_apps () ->
+  [ ].
+
+type () ->
+  supervisor.
+
+%%====================================================================
+% supervisor callbacks
+%%====================================================================
+init ([Config]) ->
+  % default to one process per scheduler
+  Number =
+    proplists:get_value (number, Config, erlang:system_info(schedulers)),
+
+  { ok,
+   {
+      {one_for_one, 10, 10},
+      [
+        { ?POOL,
+         { mondemand_backend_worker_pool_sup, start_link,
+           [ ?POOL,
+             mondemand_backend_worker,
+             Number,
+             ?MODULE
+           ]
+         },
+         permanent,
+         2000,
+         supervisor,
+         [ ]
+        }
+      ]
+    }
+  }.
+
+%%====================================================================
+%% mondemand_backend_worker callbacks
+%%====================================================================
+create (_Config) ->
+  {ok, #state { } }.
+
+connected (_State) ->
+  true. % always connected
+
+connect (State) ->
+  {ok, State}.
+
+send (State, _Data) ->
+  { ok, State }.
+
+destroy (_) ->
+  ok.
