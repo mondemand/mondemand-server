ce4c593bdffd3091961f6ac2161ae755bc43f855
Anthony Molinaro <anthonym@alumni.caltech.edu>
2017-02-10 23:04:12 +0000

    add a perf trace collator, WIP doesn't do much

---
+++ src/mondemand_backend_performance_collate.erl
@@ -0,0 +1,143 @@
+-module (mondemand_backend_performance_collate).
+
+-behaviour (supervisor).
+-behaviour (mondemand_server_backend).
+-behaviour (mondemand_backend_worker).
+
+%% mondemand_backend_worker callbacks
+-export ([ create/1,
+           connected/1,
+           connect/1,
+           send/2,
+           destroy/1
+         ]).
+
+%% mondemand_server_backend callbacks
+-export ([ start_link/1,
+           process/1,
+           required_apps/0,
+           type/0
+         ]).
+
+%% supervisor callbacks
+-export ([ init/1 ]).
+
+-define (POOL, md_perf_collate_pool).
+-record (state, { }).
+
+%%====================================================================
+%% mondemand_server_backend callbacks
+%%====================================================================
+start_link (Config) ->
+  supervisor:start_link ( { local, ?MODULE }, ?MODULE, [Config]).
+
+process (Event) ->
+  mondemand_backend_worker_pool_sup:process (?POOL, Event).
+
+required_apps () ->
+  [ ].
+
+type () ->
+  supervisor.
+
+%%====================================================================
+% supervisor callbacks
+%%====================================================================
+init ([Config]) ->
+  % default to one process per scheduler
+  Number =
+    proplists:get_value (number, Config, erlang:system_info(schedulers)),
+  AgeSeconds =
+    proplists:get_value (max_age, Config, 300),
+  FlushIntervalSeconds =
+    proplists:get_value (flush_interval, Config, 10),
+
+  { ok,
+   {
+      {one_for_one, 10, 10},
+      [
+        { mondemand_backend_performance_db,
+          { mondemand_backend_performance_db, start_link,
+            [AgeSeconds, FlushIntervalSeconds] },
+          permanent,
+          2000,
+          worker,
+          [ mondemand_backend_performance_db ]
+        },
+        { ?POOL,
+         { mondemand_backend_worker_pool_sup, start_link,
+           [ ?POOL,
+             mondemand_backend_worker,
+             Number,
+             ?MODULE
+           ]
+         },
+         permanent,
+         2000,
+         supervisor,
+         [ ]
+        }
+      ]
+    }
+  }.
+
+%%====================================================================
+%% mondemand_backend_worker callbacks
+%%====================================================================
+create (_Config) ->
+  {ok, #state { }}.
+
+connected (_State) ->
+  true. % always connected
+
+connect (State) ->
+  {ok, State}.
+
+send (State = #state { }, Data) ->
+  % use the timestamp of the event to see if it might be time to check
+  % for new indices
+  Timestamp = mondemand_event:receipt_time (Data),
+
+  % convert messages to json
+  PerfMsg = mondemand_event:msg (Data),
+  Id = mondemand_perfmsg:id(PerfMsg),
+  Context = mondemand_server_util:construct_context_string(
+              lists:keysort(1,mondemand_perfmsg:context (PerfMsg)),
+              ":","\t"),
+  Lines =
+    list_to_binary(
+      [
+        begin
+          Start = mondemand_perfmsg:timing_start_time (L),
+          End = mondemand_perfmsg:timing_end_time (L),
+          [
+            io_lib:format ("~b\t"
+                           "~s\t"
+                           "~s\t"
+                           "~b\t"
+                           "~b\t"
+                           "~b\t"
+                           "~s\t",
+                           [ Timestamp,
+                             mondemand_perfmsg:caller_label (PerfMsg),
+                             mondemand_perfmsg:timing_label (L),
+                             Start,
+                             End,
+                             (End - Start),
+                             Id
+                           ]),
+            Context,
+            [ <<"\tsource_ip:">>,
+              lwes_util:ip2bin(mondemand_event:sender_ip (Data)),
+              "\n" ]
+          ]
+        end
+        || L <- mondemand_perfmsg:timings (PerfMsg)
+      ]
+    ),
+
+  mondemand_backend_performance_db:add_trace (Id, Timestamp, Lines),
+  { ok, State }.
+
+destroy (_) ->
+  ok.
--- src/mondemand_backend_performance_db.erl
+++ src/mondemand_backend_performance_db.erl
@@ -3,7 +3,11 @@
 -behaviour (gen_server).
 
 %% API
--export ([start_link/0]).
+-export ([start_link/2,
+          add_trace/3,
+          clear_all/0,
+          stats/0
+         ]).
 
 %% gen_server callbacks
 -export ( [ init/1,
@@ -14,26 +18,56 @@
             code_change/3
           ]).
 
--record (state, {}).
+-record (state, { timer, age_millis, flush_interval_millis }).
+-define (TIME_TABLE, md_be_perf_id2ts).
+-define (PERF_TABLE, md_be_perf_ids).
+
+-record (time, {id, timestamp}).
+-record (traces, {id, trace}).
 
 %-=====================================================================-
 %-                                  API                                -
 %-=====================================================================-
-start_link() ->
-  gen_server:start_link ({local, ?MODULE}, ?MODULE, [], []).
+start_link(AgeSeconds, FlushIntervalSeconds) ->
+  gen_server:start_link ({local, ?MODULE}, ?MODULE, [AgeSeconds, FlushIntervalSeconds], []).
+
+add_trace (PerfId, Timestamp, Trace) ->
+  ets:insert (?TIME_TABLE, #time {id = PerfId, timestamp = Timestamp}),
+  ets:insert (?PERF_TABLE, #traces { id = PerfId, trace = Trace }).
+
+clear_all () ->
+  ets:delete_all_objects (?TIME_TABLE),
+  ets:delete_all_objects (?PERF_TABLE).
+
+stats () ->
+  [ ets:info(?TIME_TABLE,size), ets:info (?PERF_TABLE, size) ].
 
 %-=====================================================================-
 %-                        gen_server callbacks                         -
 %-=====================================================================-
-init([]) ->
-  ets:new (md_be_perf_id2pid, [ set,
-                                public,
-                                named_table,
-                                {write_concurrency, true},
-                                {read_concurrency, true},
-                                {keypos, 1}
-                              ]),
-  { ok, #state{} }.
+init([AgeSeconds, FlushIntervalSeconds]) ->
+  ets:new (?TIME_TABLE, [ set,
+                          public,
+                          named_table,
+                          {write_concurrency, true},
+                          {read_concurrency, true},
+                          {keypos, 2}
+                        ]),
+  ets:new (?PERF_TABLE, [ bag,
+                          public,
+                          named_table,
+                          {write_concurrency, true},
+                          {read_concurrency, true},
+                          {keypos, 2}
+                        ]),
+  timer:send_after (
+     mondemand_util:millis_to_next_round_second(),
+     ?MODULE,
+     flush),
+
+  {ok, #state{ timer = undefined,
+               age_millis = AgeSeconds * 1000,
+               flush_interval_millis = FlushIntervalSeconds * 1000 }}.
 
 handle_call (_Request, _From, State = #state { }) ->
   {reply, ok, State }.
@@ -41,6 +75,16 @@ handle_call (_Request, _From, State = #state { }) ->
 handle_cast (_Request, State = #state { }) ->
   {noreply, State}.
 
+handle_info (flush, State = #state { timer = undefined,
+                                     age_millis = MaxAge,
+                                     flush_interval_millis = FlushInterval
+                                   }) ->
+  {ok, TRef} = timer:send_interval (FlushInterval, ?MODULE, flush),
+  flush (MaxAge),
+  {noreply, State#state { timer = TRef }};
+handle_info (flush, State = #state { age_millis = MaxAge }) ->
+  flush (MaxAge),
+  {noreply, State};
 handle_info (_Info, State = #state {}) ->
   {noreply, State}.
 
@@ -49,3 +93,33 @@ terminate (_Reason, _State) ->
 
 code_change (_OldVsn, State, _Extra) ->
   {ok, State}.
+
+flush(MaxAge) ->
+  Now = mondemand_util:millis_since_epoch(),
+  Then = Now - MaxAge,
+  OldIds =
+    ets:select (?TIME_TABLE,
+                [{{time,'$1','$2'},[{'<','$2',Then}],['$1']}]),
+  NumberToFlush = length(OldIds),
+  clear (OldIds),
+  Finished = mondemand_util:millis_since_epoch(),
+  case NumberToFlush > 0 of
+    true ->
+      io:format ("flushing ~p in ~p millis~n",[NumberToFlush, Finished - Now]);
+    false ->
+      ok
+  end,
+  ok.
+
+clear ([]) ->
+  ok;
+clear ([Id | Ids]) ->
+  case ets:lookup (?PERF_TABLE, Id) of
+    [] ->
+      ok;
+    L when is_list (L) ->
+%      [ io:format ("~s",[E]) || #traces { trace = E } <- L ],
+      ets:delete (?PERF_TABLE, Id),
+      ets:delete (?TIME_TABLE, Id)
+  end,
+  clear (Ids).
